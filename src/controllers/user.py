0f622d8f425bcc87de15da9184daff95f004cbc2
Bernardo <bernardo.bcruz@gmail.com>
2024-11-26 11:59:23 -0300

    feat: creates the user entity

---
+++ src/controllers/user.py
@@ -0,0 +1,20 @@
+from ..models.usuario import User
+
+
+class UsuarioController:
+
+  @classmethod
+  def listar_usuarios(cls, user: User):
+    return
+
+  @classmethod
+  def cadastrar_usuario(cls, user: User):
+    return
+
+  @classmethod
+  def editar_usuario(cls, user: User):
+    return
+
+  @classmethod
+  def excluir_usuario(cls, user: User):
+    return
---
+++ src/models/user.py
@@ -0,0 +1,7 @@
+class User:
+  def __init__(self, cpf: str, email: str, nome: str, data_nasc: str, telefone: str):
+    self.cpf = cpf
+    self.email = email
+    self.nome = nome
+    self.data_nasc = data_nasc
+    self.telefone = telefone
